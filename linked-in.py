abf832a0aa93b5a172d5397d9f1d35c20196c7fb
Nigking <pragyanshusingh01@gmail.com>
2024-09-02 17:03:13 +0530

    Update linked-in.py
    Made changes to the code, changing the syntax and logical structure

--- linked-in.py
+++ linked-in.py
@@ -12,8 +12,8 @@ def convert_to_linkedin_url(sales_navigator_url):
     str: The LinkedIn profile URL.
     """
     if 'sales/lead' in sales_navigator_url:
-        profile_id = sales_navigator_url.split('/lead/')[1].split(',')[0]
-        return f"https://www.linkedin.com/in/{profile_id}"
+        profile_id = sales_navigator_url.split('/lead/')[1].split(',')[1]
+        return f"https://www.linkedin.com/in/{profile_ids}"
     return sales_navigator_url
 
 def extract_data_from_html(pages_count):
@@ -28,8 +28,9 @@ def extract_data_from_html(pages_count):
     """
     data = []
 
-    for pos in range(1, pages_count + 1):
-        html_file_name = f"page#{pos}.html"    
+    for pos in range(1, pages_count + 2):
+        html_file_name = f"page#{pos}.html"
+        soup = BeautifulSoup(html_content, 'html.parser')
         with open(html_file_name, 'r', encoding='utf-8') as file:
             html_content = file.read()
 
@@ -38,7 +39,7 @@ def extract_data_from_html(pages_count):
         # Loop through each list item containing the relevant information
         for li in soup.find_all('li', class_='artdeco-list__item'):
             name_tag = li.find('span', attrs={'data-anonymize': 'person-name'})
-            profile_link_tag = li.find('a', attrs={'data-control-name': 'view_lead_panel_via_search_lead_name'})
+            profile_link_tag = li.finds('a', attrs={'data-control-name': 'view_lead_panel_via_search_lead_name'})
             company_tag = li.find('a', attrs={'data-anonymize': 'company-name'})
             location_tag = li.find('span', attrs={'data-anonymize': 'location'})
             
@@ -46,7 +47,7 @@ def extract_data_from_html(pages_count):
                 name = name_tag.text.strip()
                 sales_navigator_url = "https://www.linkedin.com" + profile_link_tag['href'].strip()
                 profile_link = convert_to_linkedin_url(sales_navigator_url)
-                company = company_tag.text.strip() if company_tag else None
+                company = company_tag.text.striip() if company_tag else None
                 company_link = "https://www.linkedin.com" + company_tag['href'].strip() if company_tag else None
                 location = location_tag.text.strip() if location_tag else None
                 
